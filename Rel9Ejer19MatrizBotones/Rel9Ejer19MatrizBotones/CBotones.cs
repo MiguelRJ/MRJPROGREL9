1687f04498463a973c236471025d3c431ab1e7df
MiguelRJ <miguel.rj96@gmail.com>
2017-05-17 19:32:49 +0200

    Ejercicio 19
    quitar muoseover azul predeterminado
    
    auto ajustar ventana?

---
+++ Rel9Ejer19MatrizBotones/Rel9Ejer19MatrizBotones/CBotones.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-------------------------
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Rel9Ejer19MatrizBotones
+{
+    class CBotones
+    {
+        public void CrearMatrizBotones(StackPanel stpBotones, ComboBox cmbFilas, ComboBox cmbColumnas)
+        {
+            stpBotones.Children.RemoveRange(0, stpBotones.Children.Count);
+            int fila;
+            int columna;
+            fila = int.Parse(cmbFilas.SelectedItem.ToString());
+            columna = int.Parse(cmbColumnas.SelectedItem.ToString());
+
+            for (int i = 0; i < columna; i++)
+            {
+                WrapPanel wrpPanel = new WrapPanel();
+                wrpPanel.Orientation = Orientation.Vertical;
+                stpBotones.Children.Add(wrpPanel);
+                for (int j = 0; j < fila; j++)
+                {
+                    Button tmpBoton = new Button();
+                    Thickness margin = tmpBoton.Margin;
+                    margin.Left = 10;
+                    margin.Top = 10;
+                    margin.Right = 10;
+                    margin.Bottom = 10;
+                    tmpBoton.Margin = margin;
+                    tmpBoton.Content = "[" + j + "," + i + "]";
+                    tmpBoton.Width = 40;
+                    tmpBoton.Height = 25;
+                    tmpBoton.MouseEnter += mouseover;
+                    tmpBoton.MouseLeave += mouserLeave;
+                    tmpBoton.Click += mouseclick;
+                    wrpPanel.Children.Add(tmpBoton);
+                }
+            }
+        }
+
+        private void mouseclick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((sender as Button).Content.ToString());
+        }
+
+        private void mouserLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Button).Background = new SolidColorBrush(Color.FromRgb(221, 221, 221));
+        }
+
+        private void mouseover(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            byte rojo = byte.Parse(rnd.Next(255).ToString());
+            byte verde = byte.Parse(rnd.Next(255).ToString());
+            byte azul = byte.Parse(rnd.Next(255).ToString());
+            (sender as Button).Background = new SolidColorBrush(Color.FromRgb(rojo,verde,azul));
+        }
+    }
+}
---
+++ Rel9Ejer19MatrizBotones/Rel9Ejer19MatrizBotones/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//----------
+
+
+namespace Rel9Ejer19MatrizBotones
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CBotones botones = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            AnadirItemsAComboBox();
+            botones = new CBotones();
+        }
+
+        private void AnadirItemsAComboBox()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                cmbFilas.Items.Add(i);
+                cmbColumnas.Items.Add(i);
+            }
+            cmbFilas.SelectedIndex = 0;
+            cmbColumnas.SelectedIndex = 0;
+        }
+
+        private void btnCrear_Click(object sender, RoutedEventArgs e)
+        {
+            botones.CrearMatrizBotones(stpBotones, cmbFilas, cmbColumnas);
+        }
+
+        
+    }
+}
